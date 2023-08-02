c90741b180018b86a7963f33a7a310e45627e717
Eial Kestelman <eial.kestelman@gmail.com>
2023-08-01 21:12:20 -0400

    consolidate test cases into single file

--- test.py
+++ test.py
@@ -22,6 +22,11 @@ def save_output(results, fname):
 
 if __name__ == "__main__":
   new_save = False    # Use cmd args to change?
+  if new_save:
+    cases = {}
+  else:
+    with open("test_cases.txt") as f:
+      cases = json.load(f)
   files = ["bimodal_trad.txt", "config.txt", "constant.txt", "even_peaks.txt", "high_cont_low_roi.txt"]
   for fname in files:
     with open(fname) as f:
@@ -30,19 +35,25 @@ if __name__ == "__main__":
       try:
         for i in range(1,3):
           results = main.ret_plan(args, i)
-          save_output(results, 'test'+str(i)+fname)
+          #save_output(results, 'test'+str(i)+fname)
+          cases[fname+str(i)] = results
         print("...Saved new test cases for "+fname)
       except KeyError as ke:
-        print("KeyError:", ke, " on file", fname)
+        print("KeyError:", ke, "on file", fname)
     else:
       results = [None, None]
       try:
         for i in range(1,3):
-          with open("test"+str(i)+fname) as f:
-            results[i-1] = json.load(f)
+          #with open("test"+str(i)+fname) as f:
+          #  results[i-1] = json.load(f)
+          results[i-1] = cases[fname+str(i)]
           ret_plan_test(args, i, results[i-1])
       except FileNotFoundError as fnfe:
         print(fnfe)
+      except KeyError as ke:
+        print("KeyError", ke, "on file", fname)
+  if new_save:
+    save_output(cases, "test_cases.txt")  # Consider pretty print
 
 
 
