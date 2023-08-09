7a1876bb1cae0172e15c78ec7a9e3acb1893fd93
Eial Kestelman <eial.kestelman@gmail.com>
2023-08-08 20:23:51 -0400

    get all test cases from a single json

--- test.py
+++ test.py
@@ -3,12 +3,16 @@
 # the desired inputs for the main program. To save test case results, set \
 # new_save = True. To test the program against previously saved answers, set \
 # new_save = False.
+# What needs to be tested? Salaries/contributions above and below limit. Non
+# zero starting balance, pension. Increased limit over 50.
 
 import json
 import main
+import sys
 
 # Test arrays output by ret_plan?
 # Clean up main so that we can also just test the summary results?
+# Way of adding test case from file?
 
 def ret_plan_test(args, rothorder, result):
   try:
@@ -19,44 +23,55 @@ def ret_plan_test(args, rothorder, result):
   except AssertionError as ae:
     print("AssertionError", ae)
 
-def save_output(results, fname):
+def save_output(results, fname):   # Pointless funcification?
   with open(fname, 'w') as f:
-    json.dump(results, f)
+    json.dump(results, f, indent=2)
+
+def overwrite():
+  try:
+    if sys.argv[1] == "new":
+      print("Are you sure you want to overwrite previous test cases? (y/n)")
+      answer = input()
+      if answer == "y":
+        return True
+      else:
+        return False
+      #  raise KeyboardInterrupt  # just an idea
+    else:
+      return False
+  except IndexError:
+    return False
 
 if __name__ == "__main__":
   new_save = False    # Use cmd args to change?
+  new_save = overwrite()
+  with open("test_cases.txt") as f:
+    cases = json.load(f)
+  ins = cases["ins"]      # Inputs
   if new_save:
-    cases = {}
-  else:
-    with open("test_cases.txt") as f:
-      cases = json.load(f)
-  files = ["bimodal_trad.txt", "config.txt", "constant.txt", "even_peaks.txt", "high_cont_low_roi.txt"]
-  for fname in files:
-    with open(fname) as f:
-      args = json.load(f)
-    if new_save:
+    outs = {}
+    for key in ins:
       try:
         for i in range(1,3):
-          results = main.ret_plan(args, i)
-          #save_output(results, 'test'+str(i)+fname)
-          cases[fname+str(i)] = results
-        print("...Saved new test cases for "+fname)
+          results = main.ret_plan(ins[key], i)
+          outs[key+str(i)] = results
+        print("...Saving new test cases for "+key)
       except KeyError as ke:
-        print("KeyError:", ke, "on file", fname)
-    else:
+        #print("KeyError:", ke, "on file", fname)
+        print("KeyError:", ke, "on case", key)
+    cases["outs"] = outs
+    save_output(cases, "test_cases.txt")
+  else:
+    outs = cases["outs"]  # Outputs
+    for key in ins:
       results = [None, None]
       try:
+        args = ins[key]
         for i in range(1,3):
-          #with open("test"+str(i)+fname) as f:
-          #  results[i-1] = json.load(f)
-          results[i-1] = cases[fname+str(i)]
+          results[i-1] = outs[key+str(i)]
           ret_plan_test(args, i, results[i-1])
-      except FileNotFoundError as fnfe:
-        print(fnfe)
       except KeyError as ke:
-        print("KeyError", ke, "on file", fname)
-  if new_save:
-    save_output(cases, "test_cases.txt")  # Consider pretty print
+        print("KeyError", ke, "on case", key)
 
 
 
