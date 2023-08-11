e40af3b6298af5e0fbeb8f1c3c4a9acd04a228b2
Eial Kestelman <eial.kestelman@gmail.com>
2023-08-11 00:44:27 -0400

    two line graph like in main; set up axes for pie graphs

--- iplot.py
+++ iplot.py
@@ -18,7 +18,11 @@ except:
 rothorder = 1
 vals['rothorder'] = rothorder   # But would we prefer simultaneous plots?
 
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
+fig = plt.figure()
+ax = plt.subplot(211)
+ax1 = plt.subplot(223)
+ax2 = plt.subplot(224)
 under = False     # Place textboxes under graph (or to the right)
 if under:
   fig.subplots_adjust(bottom=.4)  # Make this a function of len(vals?)
@@ -28,7 +32,11 @@ else:
                                                  # years from today, but not
                                                  # yet operational.
 #year = np.arange(0, vals["work years"]+1, 1)
-line, = plt.plot(main.ret_plan(vals, rothorder)[0])  # ax.plot?
+line, = ax.plot(main.ret_plan(vals, rothorder)[0])
+vals["rothorder"] = 2
+line2, = ax.plot(main.ret_plan(vals, rothorder)[0])
+#pie1 = ax1.pie  # Summary function would be very handy here
+# Could be plt.plot if only one axis object, else ax needs to be specified.
 
 del vals['normalize'], vals['work years'], vals['ret years']
 # This breaks something: KeyError: 'work years' line 38
@@ -39,12 +47,15 @@ def update(arg):     # Alternative: 0 args in update() and remove next line
   newvals = {v : eval(boxes[v].text) for v in vals}
   budget = main.ret_plan(newvals, rothorder)[0] # newvals -> vals
                                                 # if alt method
+  newvals["rothorder"] = newvals["rothorder"] % 2 + 1
+  budget2 = main.ret_plan(newvals, rothorder)[0]
   newvals['work years'] = newvals['ret age'] - newvals['age']
   years = np.arange(0, newvals['work years']+1, 1)
   #line.set_ydata(budget)   # Insufficient of we change age/ret age/work years
   line.set_data(years, budget)
-  high = np.max(budget)
-  low = np.min(budget)
+  line2.set_data(years, budget2)
+  high = max(np.max(budget), np.max(budget2))
+  low = min(np.min(budget), np.min(budget2))
   diff = high-low
   ax.set_ylim(low-diff*.05, high+diff*.05)
   ax.set_xlim(-.05*newvals['work years'], 1.05*newvals['work years'])
